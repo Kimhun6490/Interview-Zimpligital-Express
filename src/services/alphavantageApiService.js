adeedb2be3f047d91d629343788d4268a722f377
DESKTOP-P8O5A4B\Kim <kimhunmalai@gmail.com>
2024-06-07 15:35:11 +0700

    feat: add cache util

--- src/services/alphavantageApiService.js
+++ src/services/alphavantageApiService.js
@@ -1,4 +1,5 @@
 const { ALPHA_VANTAGE_BASE_URL } = require('../constants')
+const cache = require('../utils/cache');
 const { alphavantageStockPayloadMapper } = require('../utils/stockUtil')
 const axios = require('axios')
 const client = axios.create({
@@ -11,12 +12,18 @@ const client = axios.create({
 
 exports.queryStockBySymbol = async (symbol) => {
   try {
-    const queryParams = `function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=JR4RGUPGQZYDNL8A`
+    const cachedData = cache.get(symbol);
+    if (cachedData) return cachedData;
+
+    const queryParams = `function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=C2T65JUZI7YVUUT0`
     const result = await client.get(`/query?${queryParams}`)
     const data = result.data['Time Series (5min)']
     const firstKey = Object.keys(data)[0]
     const firstElement = data[firstKey]
-    return alphavantageStockPayloadMapper(firstElement)
+
+    const stockData = alphavantageStockPayloadMapper(firstElement)
+    cache.set(symbol, stockData);
+    return stockData
   } catch (error) {
     console.log(error)
     return null
